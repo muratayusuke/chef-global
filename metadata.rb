fb14ef9bcc29bfe076c8f255020173717c8f3253
muratayusuke <info@muratayusuke.com>
2015-02-08 17:25:31 +0900

    bump up version

--- metadata.rb
+++ metadata.rb
@@ -2,8 +2,8 @@ name             'global'
 maintainer       'Yusuke Murata'
 maintainer_email 'info@muratayusuke.com'
 license           "Apache 2.0"
-description      'Installs/Configures global'
+description      'Installs/Configures global. http://www.gnu.org/software/global/'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 supports "ubuntu"
